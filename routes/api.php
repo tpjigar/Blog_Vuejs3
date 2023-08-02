13c173372844f9d74deca4e8cae5611e516b4e28
jigarpatel <jigar.patel@planleft.com>
2023-08-02 19:03:42 +0530

    post api and fetch data in vue component

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('posts', [PostController::class, 'index']);
+Route::apiResource('posts', \App\Http\Controllers\Api\PostController::class);
