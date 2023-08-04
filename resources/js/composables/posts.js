1c9ea7ecc193b1d22bab9a070e8fd4b2c4cd805f
jigarpatel <jigar.patel@planleft.com>
2023-08-04 20:07:35 +0530

    Form submit Loading Indicator

--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -6,6 +6,7 @@ export default function usePosts() {
     const posts = ref({})
     const router = useRouter()
     const validationErrors = ref({})
+    const isLoading = ref(false)
 
     const getPosts = async(
         page = 1,
@@ -24,6 +25,10 @@ export default function usePosts() {
     }
 
     const storePost = async (post) => {
+        if (isLoading.value) return;
+        isLoading.value = true
+        validationErrors.value = {}
+
         axios.post('/api/posts', post)
             .then(response => {
                 router.push({ name: 'posts.index' })
@@ -31,11 +36,12 @@ export default function usePosts() {
             .catch(error => {
                 if (error.response?.data) {
                     validationErrors.value = error.response.data.errors
+                    isLoading.value = false
                 }
             })
     }
 
 
-    return { posts, getPosts, storePost, validationErrors }
+    return { posts, getPosts, storePost, validationErrors, isLoading }
 
 }
