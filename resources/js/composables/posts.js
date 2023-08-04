5ac953754d4b400db544f781c45c6ca21f8fcfb9
jigarpatel <jigar.patel@planleft.com>
2023-08-04 16:42:39 +0530

    table sorting

--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -4,8 +4,17 @@ import axios from "axios";
 export default function usePosts() {
     const posts = ref({})
 
-    const getPosts = async(page = 1, category = '') =>{
-        axios.get('api/posts?page='+ page +'&category='+ category)
+    const getPosts = async(
+        page = 1,
+        category = '',
+        order_column = 'created_at',
+        order_direction = 'desc'
+    ) =>{
+        axios.get('api/posts?page='+ page +
+            '&category='+ category +
+            '&order_column='+ order_column +
+            '&order_direction='+ order_direction
+        )
             .then(response => {
                 posts.value = response.data;
             })
