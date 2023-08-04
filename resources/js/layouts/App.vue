bd156b80dfc94f72294c5ad477409d36b5ea060d
jigarpatel <jigar.patel@planleft.com>
2023-08-04 17:55:10 +0530

    routing dynamic

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -16,10 +16,10 @@
 
                         <!-- Navigation Links -->
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                            <router-link to="/" active-class="border-b-2 border-indigo-400" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                            <router-link :to="{ name: 'posts.index' }" active-class="border-b-2 border-indigo-400" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
                                 Posts
                             </router-link>
-                            <router-link to="/posts/create" active-class="border-b-2 border-indigo-400" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                            <router-link :to="{ name: 'posts.create' }" active-class="border-b-2 border-indigo-400" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
                                 Create Post
                             </router-link>
                         </div>
@@ -32,7 +32,7 @@
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
+                    {{ currentPageTitle }}
                 </h2>
             </div>
         </header>
@@ -51,3 +51,11 @@
         </main>
     </div>
 </template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const currentPageTitle = computed(() => route.meta.title)
+</script>
---
+++ resources/js/routes/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import PostsIndex from "../components/Posts/Index.vue";
+import PostsCreate from "../components/Posts/create.vue";
+
+const routes = [
+    {
+        path: '/',
+        name: 'posts.index',
+        component: PostsIndex,
+        meta: { title: 'Posts' }
+    },
+    {
+        path: '/posts/create',
+        name: 'posts.create',
+        component: PostsCreate,
+        meta: { title: 'Add new Post' }
+    },
+]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
