aa207bd6d1ea1428a426d9180d62479f666cb0cd
jigarpatel <jigar.patel@planleft.com>
2023-08-04 17:44:02 +0530

    use Vue-router for menu

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard')->name('dashboard');
+//Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/{any?}', 'dashboard')
+    ->where('any', '.*');
