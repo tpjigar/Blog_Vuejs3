0969afbec41f833e3e51b077ff3d0e8867f9996c
jigarpatel <jigar.patel@planleft.com>
2023-08-05 22:51:49 +0530

    table column filtering

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -2,7 +2,8 @@
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
             <div class="mb-4">
-                <select v-model="selectedCategory" class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <select v-model="search_category"
+                        class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                     <option value="" selected>-- Filter by category --</option>
                     <option v-for="category in categories" :value="category.id" :key="category.id">
                         {{ category.name }}
@@ -13,9 +14,32 @@
                 <thead>
                 <tr>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('id')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
+                        <input v-model="search_id" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by ID">
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <input v-model="search_title" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Title">
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <select v-model="search_category" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <option value="" selected>-- all categories --</option>
+                            <option v-for="category in categories" :value="category.id">
+                                {{ category.name }}
+                            </option>
+                        </select>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <input v-model="search_content" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Content">
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                    <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                </tr>
+                <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer"
+                             @click="updateOrdering('id')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                 :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
                                 ID
                             </div>
                             <div class="select-none">
@@ -31,9 +55,11 @@
                         </div>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('title')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
+                        <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer"
+                             @click="updateOrdering('title')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                 :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
                                 TITLE
                             </div>
                             <div class="select-none">
@@ -49,15 +75,19 @@
                         </div>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
+                        <span
+                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                        <span
+                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('created_at')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
+                        <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer"
+                             @click="updateOrdering('created_at')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                 :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
                                 Created at
                             </div>
                             <div class="select-none">
@@ -73,7 +103,8 @@
                         </div>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Actions</span>
+                        <span
+                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Actions</span>
                     </th>
                 </tr>
                 </thead>
@@ -83,7 +114,7 @@
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.title }}</td>
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.category }}</td>
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.content }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.created_at  }}</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.created_at }}</td>
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                         <router-link :to="{ name: 'posts.edit', params: { id: post.id } }">Edit</router-link>
                         <a href="#" @click.prevent="deletePost(post.id)" class="ml-2">Delete</a>
@@ -92,28 +123,42 @@
 
                 </tbody>
             </table>
-            <TailwindPagination :data="posts" @pagination-change-page="page => getPosts(page, selectedCategory)" class="mt-4" />
+            <TailwindPagination :data="posts" @pagination-change-page="page => getPosts(page, search_category)"
+                                class="mt-4"/>
         </div>
     </div>
 </template>
 
 <!--Vue 3 Composable API start -->
 <script setup>
-import { onMounted, ref, watch } from "vue";
-import { TailwindPagination } from 'laravel-vue-pagination';
+import {onMounted, ref, watch} from "vue";
+import {TailwindPagination} from 'laravel-vue-pagination';
 import usePosts from "@/composables/posts";
 import useCategories from "@/composables/categories";
 
 const selectedCategory = ref('')
+const search_category = ref('')
+const search_id = ref('')
+const search_title = ref('')
+const search_content = ref('')
+const search_global = ref('')
 const orderColumn = ref('created_at')
 const orderDirection = ref('desc')
-const { posts, getPosts, deletePost } = usePosts()
-const { categories, getCategories } = useCategories()
+const {posts, getPosts, deletePost} = usePosts()
+const {categories, getCategories} = useCategories()
 
 const updateOrdering = (column) => {
     orderColumn.value = column
     orderDirection.value = (orderDirection.value === 'asc') ? 'desc' : 'asc'
-    getPosts(1, selectedCategory.value, orderColumn.value, orderDirection.value)
+    getPosts(
+        1,
+        search_category.value,
+        search_id.value,
+        search_title.value,
+        search_content.value,
+        orderColumn.value,
+        orderDirection.value
+    )
 }
 
 onMounted(() => {
@@ -121,8 +166,17 @@ onMounted(() => {
     getCategories()
 })
 
-watch(selectedCategory, (current, previous) => {
-    getPosts(1, current)
+watch(search_category, (current, previous) => {
+    getPosts(1, current, search_id.value, search_title.value, search_content.value)
+})
+watch(search_id, (current, previous) => {
+    getPosts(1, search_category.value, current, search_title.value, search_content.value)
+})
+watch(search_title, (current, previous) => {
+    getPosts(1, search_category.value, search_id.value, current, search_content.value)
+})
+watch(search_content, (current, previous) => {
+    getPosts(1, search_category.value, search_id.value, search_title.value, current)
 })
 </script>
 <!--Vue 3 Composable API end -->
